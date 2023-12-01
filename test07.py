6db45d3b37621e9dec3ff5023f5b1e9d7d423e1a
HoseinAttarzadeh <152622252+HoseinAttarzadeh@users.noreply.github.com>
2023-12-01 17:21:49 +0330

    Add files via upload

---
+++ test07.py
@@ -0,0 +1,14 @@
+op = input("Enter op(+,-,*,/): ")
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+if op == "+":
+    print(a+b)
+
+if op == "-":
+    print(a-b)   
+if op == "*":
+    print(a*b)
+if op == "/":
+    print(a/b)
+
---
+++ test08.py
@@ -0,0 +1,8 @@
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+c = int(input("Enter number c: "))
+
+if a+b>c and b+c>a and a+c>b:
+    print("mosallas rasm mishavad")
+else:
+    print("mosallas rasm nemishavad")
